a29aed6566675e6910b8b9d01b6dbcb3707da8bf
DUBIPSSI <DUBIPSSI@users.noreply.github.com>
2024-04-23 15:42:13 +0200

    image event

--- scripts/index.js
+++ scripts/index.js
@@ -60,7 +60,7 @@ class Event {
                 <defs>
             </defs>
                 <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
-                    <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-207.000000, -257.000000)" fill="#fec816">
+                    <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-207.000000, -257.000000)" fill="var(--mainColor)">
                         <path d="M231,273 C229.896,273 229,272.104 229,271 C229,269.896 229.896,269 231,269 C232.104,269 233,269.896 233,271 C233,272.104 232.104,273 231,273 L231,273 Z M223,273 C221.896,273 221,272.104 221,271 C221,269.896 221.896,269 223,269 C224.104,269 225,269.896 225,271 C225,272.104 224.104,273 223,273 L223,273 Z M215,273 C213.896,273 213,272.104 213,271 C213,269.896 213.896,269 215,269 C216.104,269 217,269.896 217,271 C217,272.104 216.104,273 215,273 L215,273 Z M223,257 C214.164,257 207,263.269 207,271 C207,275.419 209.345,279.354 213,281.919 L213,289 L220.009,284.747 C220.979,284.907 221.977,285 223,285 C231.836,285 239,278.732 239,271 C239,263.269 231.836,257 223,257 L223,257 Z" id="comment-3" sketch:type="MSShapeGroup"></path>
                     </g>
                 </g>
@@ -136,6 +136,25 @@ document.addEventListener('click', (e) => {
         const ville = eventForm.querySelector('#eventVille').value;
         const description = eventForm.querySelector('#textar').value;
         const capacite = parseInt(eventForm.querySelector('#eventCapacite').value);
+        const eventImage = document.getElementById('eventImage');
+        var base64String = null;
+        if (eventImage.files.length > 0) {
+            const file = eventImage.files[0];
+            const reader = new FileReader();
+
+            reader.onloadend = function () {
+                console.log("Fichier chargé avec succès :");
+                const base64String = reader.result;
+                console.log(base64String);
+            };
+
+            reader.onerror = function (error) {
+                console.log('Erreur lors de la lecture du fichier:', error);
+            };
+
+            reader.readAsDataURL(file);
+            console.log("Début de la lecture du fichier...");
+        }
 
         console.log(nom, prix, date, ville, description, capacite);
 
@@ -146,6 +165,7 @@ document.addEventListener('click', (e) => {
             prix: prix,
             capacite: capacite,
             date: date,
+            image: base64String
         })
             .then(response => {
                 console.log("Réponse du serveur :", response.data);
@@ -162,7 +182,7 @@ document.addEventListener('click', (e) => {
         const token = getCookieValue('loginToken');
         console.log(id, token);
         axios.post('http://m2l.site:3000/post/addParticipation', {
-            event_id : id,
+            event_id: id,
             token: token
         })
             .then(response => {
@@ -177,7 +197,7 @@ document.addEventListener('click', (e) => {
         const id = e.target.closest('.eventContainer').id;
         const token = getCookieValue('loginToken');
         axios.post('http://m2l.site:3000/post/deleteParticipation', {
-            event_id : id,
+            event_id: id,
             token: token
         })
             .then(response => {
@@ -225,7 +245,7 @@ async function leBang() {
             const formattedDate = `${day}/${month}/${year}`;
 
             // Supposons que la classe Event soit définie ailleurs dans votre code
-            let eventObj = new Event(data.nom, data.description, "assets/imgEvent/tournoiFoot.png", formattedDate, data.lieu, data.prix, data.capacite, data.id);
+            let eventObj = new Event(data.nom, data.description, data.image, formattedDate, data.lieu, data.prix, data.capacite, data.id);
 
             // Supposons que eventsContainer soit défini ailleurs dans votre code
             let eventElement = eventObj.createEvent();
